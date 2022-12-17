b96538860d103d7bebe1ea2c286e0286be6ebe75
Risyad Rais <satmo54@gmail.com>
2022-12-17 17:32:03 +0700

    Submit Log Level

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,9 +1,21 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    label = {:trace, :debug, :info, :warning, :error, :fatal, :unknown}
+
+    cond do
+      level in 1..4 or (level in [0, 5] and not legacy?) -> elem(label, level)
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+
+    cond do
+      label in [:fatal, :error] -> :ops
+      label == :unknown and legacy? -> :dev1
+      label == :unknown and not legacy?  -> :dev2
+      true -> false
+    end
   end
 end
